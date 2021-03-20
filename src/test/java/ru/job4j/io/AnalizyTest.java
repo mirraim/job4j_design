2f5193a334557fe8b2fc4cb071bd3271bd6597d6
mirraim <mirraim@gmail.com>
2021-03-20 21:05:56 +0500

    3.0. Тестирование IO [#476373]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -11,17 +13,29 @@ import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenUnavailable() {
-        Analizy analizy = new Analizy();
-        analizy.unavailable("./data/server.log", "./data/unavalable.csv");
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("./data/unavalable.csv"))) {
-           lines = reader.lines().collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void whenUnavailable() throws IOException {
+        File sourse = folder.newFile("sourse.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(sourse)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println();
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
-        List<String> expected = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
-        assertEquals(lines, expected);
+        Analizy analizy = new Analizy();
+        analizy.unavailable(sourse.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("./data/unavalable.csv"))) {
+           reader.lines().forEach(rsl::append);
+        }
+        String expected = "10:57:01;10:59:01" + "11:01:02;11:02:02";
+        assertEquals(rsl.toString(), expected);
     }
 }
