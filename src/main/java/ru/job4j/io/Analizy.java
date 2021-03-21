0e92bdbeb7cef1870439b648e3d9e4126754e81c
mirraim <mirraim@gmail.com>
2021-03-22 01:14:29 +0500

    2. Анализ доступности сервера. [#476382] - обработка строки во время чтения

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,23 +1,16 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Analizy {
 
-    /**
-     * Считывает данные из файла
-     * Записывает в другой файл интервалы, когда система была неработоспособна
-     * @param source - файл-источник
-     * @param target - файл назначения
-     */
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
         PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            List<Event> events = load(reader);
-            save(writer, events);
+            List<String> lines = load(reader);
+            save(writer, lines);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,69 +22,30 @@ public class Analizy {
     }
 
     private boolean hasProblem(String status) {
-        return status.equals("400") || status.equals("500");
+        return status.startsWith("400") || status.startsWith("500");
     }
 
     private boolean isWorking(String status) {
-        return status.equals("200") || status.equals("300");
+        return status.startsWith("200") || status.startsWith("300");
     }
 
-    /**
-     * Считывает данные из файла
-     * Преобразует их в объект Event, если считанная строка соответствует условиям
-     * Собирает все ненулевые объекты Event  в список
-     * @param reader - поток ввода
-     * @return
-     */
-    private List<Event> load(BufferedReader reader) {
-        return reader.lines()
-                .map(Analizy::createEvent)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Ищет промежутки неработоспособности системы
-     * и записывает их в файл
-     * @param writer - поток вывода
-     * @param events - список событий
-     */
-    private void save(PrintWriter writer, List<Event> events) {
-        Event current = events.get(0);
-        for (Event event : events) {
-            if (isWorking(current.type) && hasProblem(event.type)) {
-                current = event;
-            } else if (hasProblem(current.type) && isWorking(event.type)) {
-                writer.println(current.date + ";" + event.date);
-                current = event;
+    private List<String> load(BufferedReader reader) throws IOException {
+        List<String> rsl = new ArrayList<>();
+        String current = reader.readLine();
+        for (String line = current; line != null; line = reader.readLine()) {
+            if (hasProblem(current) && isWorking(line)) {
+                rsl.add(current.split(" ")[1] + ";"
+                        + line.split(" ")[1]);
+                current = line;
+            } else if (hasProblem(line) && isWorking(current)) {
+                current = line;
             }
         }
+        return rsl;
     }
 
-    private static boolean isValid(String[] message) {
-        return message.length == 2;
+    private void save(PrintWriter writer, List<String> lines) {
+        lines.forEach(writer::println);
     }
 
-    /**
-     * Создает объект Event, если строка соответствует требованиям
-     * @param line
-     * @return
-     */
-    private static Event createEvent(String line) {
-        String[] message = line.split(" ");
-        if (isValid(message)) {
-           return new Event(message[0], message[1]);
-        }
-        return null;
-    }
-
-    public static class Event {
-        String type;
-        String date;
-
-        public Event(String type, String date) {
-            this.type = type;
-            this.date = date;
-        }
-    }
 }
