96bb221f8ab9e49dc0064fe192b6f249878de39a
mirraim <mirraim@gmail.com>
2021-03-23 12:34:25 +0500

    5. Валидация параметров запуска. [#476374]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +10,32 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (isValid(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile()
+                            .getName()
+                            .endsWith(args[1]))
+                            .forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean isValid(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. File extension is null");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension is null");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        return true;
     }
 }
