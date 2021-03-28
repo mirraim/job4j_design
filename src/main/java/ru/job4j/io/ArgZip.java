c33ebf0927245beca2b1fe822a1299982f2398f3
mirraim <mirraim@gmail.com>
2021-03-28 20:53:38 +0500

    5.2. Архивировать проект [#476383]

---
+++ src/main/java/ru/job4j/io/ArgZip.java
@@ -0,0 +1,75 @@
+package ru.job4j.io;
+
+public class ArgZip {
+    private String directory;
+    private String exclude;
+    private String output;
+
+    private ArgZip() {
+    }
+
+    public static ArgZip of(String[] args) {
+        ArgZip argZip = new ArgZip();
+        if (args.length == 0) {
+            throw new IllegalArgumentException("List of params is empty");
+        }
+        argZip.init(args);
+        return argZip;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public String getExclude() {
+        return exclude;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    private void init(String[] args) {
+        for (String param : args) {
+            String[] pair = param.split("=");
+            if (pair.length == 2) {
+                isDirectory(pair[0], pair[1]);
+                isExclude(pair[0], pair[1]);
+                isOutput(pair[0], pair[1]);
+            }
+        }
+        if (directory == null) {
+            throw new IllegalArgumentException("directory is empty");
+        }
+        if (exclude == null) {
+            throw new IllegalArgumentException("exclude is empty");
+        }
+        if (output == null) {
+            throw new IllegalArgumentException("output is empty");
+        }
+    }
+
+    private boolean isDirectory(String param, String value) {
+        boolean rsl = param.equals("-d");
+        if (rsl) {
+            directory = value;
+        }
+        return rsl;
+    }
+
+    private boolean isExclude(String param, String value) {
+        boolean rsl = param.equals("-e");
+        if (rsl) {
+            exclude = value;
+        }
+        return rsl;
+    }
+
+    private boolean isOutput(String param, String value) {
+        boolean rsl = param.equals("-o");
+        if (rsl) {
+            output = value;
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path: sources) {
+                File source = path.toFile();
+                zip.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.toString()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgZip params = ArgZip.of(args);
+        File target = new File(params.getOutput());
+        Predicate<Path> condition = path -> !path.endsWith(params.getExclude());
+        List<Path> source = Search.search(Paths.get(params.getDirectory()), condition);
+        new Zip().packFiles(source, target);
+    }
+}
