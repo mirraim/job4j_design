f62fa4bbb829cd9a8bbebf6dfc94aa45385937df
mirraim <mirraim@gmail.com>
2021-04-03 17:29:23 +0500

    1. Бот [#476398]

--- src/main/java/ru/job4j/io/socket/EchoServer.java
+++ src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -17,16 +17,29 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
-                    if (str.startsWith("GET /?msg=Bye")) {
-                        isEnds = true;
-                    }
+                    String response = "";
                     while (!str.isEmpty()) {
+                        if (str.startsWith("GET /?msg=Exit")) {
+                            isEnds = true;
+                        } else if (str.startsWith("GET /?msg=Hello")) {
+                            response = "Hello, dear friend";
+                        } else if (str.startsWith("GET /?msg=")) {
+                            response = getMessage(str);
+                        }
                         System.out.println(str);
                         str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(response.getBytes());
                 }
             }
         }
+    }
+
+    private static String getMessage(String str) {
+        String param = str.split(" ")[1];
+        System.out.println(param);
+        String[] message = param.split("=");
+        return message.length == 2 ? message[1] : "";
     }
 }
