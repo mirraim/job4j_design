3a8c84224bf868ce154b0d41d1ad24f5d80b4e41
mirraim <mirraim@gmail.com>
2021-04-04 16:06:02 +0500

    4. Slf4j - вывод exception. [#476370]

--- src/main/java/ru/job4j/io/socket/EchoServer.java
+++ src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -1,5 +1,9 @@
 package ru.job4j.io.socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.io.UsageLog4j;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +12,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isEnds = false;
             while (!isEnds) {
@@ -33,6 +39,8 @@ public class EchoServer {
                     out.write(response.getBytes());
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Incorrect input/output", e);
         }
     }
 
