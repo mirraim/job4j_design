6f3969794d3656c1ea3473413df3ec258516cebd
mirraim <mirraim@gmail.com>
2021-04-05 17:20:35 +0500

    2. Формат JSON [#476388]

---
+++ src/main/java/ru/job4j/io/Weapon.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+public class Weapon {
+    private int damage;
+    private String name;
+
+    public Weapon(int damage, String name) {
+        this.damage = damage;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{"
+                + "damage=" + damage
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
