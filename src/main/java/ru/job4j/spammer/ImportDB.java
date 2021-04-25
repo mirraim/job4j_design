6d2fcaee43e8fa99c3d88e0daefbc0f0245f626e
Mirraim <mirraim@gmail.com>
2021-04-25 17:09:58 +0500

    3. Загрузка базы спамеров. [#476276]

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,82 @@
+package ru.job4j.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users;
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            users = rd.lines()
+                    .map(this::textToUser)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        }
+        return users;
+    }
+
+    private User textToUser(String line) {
+        String[] data = line.split(";");
+        if (data.length == 2) {
+            return new User(data[0], data[1]);
+        }
+        return null;
+    }
+
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement(
+                                "insert into users(name, email) values (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./dump.txt");
+        db.save(db.load());
+    }
+}
