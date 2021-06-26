e7583bc9683d8d71530a9ea29c6d013a31a268a5
Mirraim <mirraim@gmail.com>
2021-06-26 23:43:26 +0500

    технический коммит

--- src/main/java/ru/job4j/gc/GCUser.java
+++ src/main/java/ru/job4j/gc/GCUser.java
@@ -1,17 +1,18 @@
 package ru.job4j.gc;
 
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+
 public class GCUser {
 
     public static void main(String[] args) throws InterruptedException {
         User user = new User();
-        //System.out.println(sizeOf(user));
+        System.out.println(sizeOf(user));
         user.setId(1264);
         user.setName("N10");
-        //System.out.println(sizeOf(user.getName()));
-        //System.out.println(sizeOf(new char[2]));
+        System.out.println(sizeOf(user.getName()));
+        System.out.println(sizeOf(new char[2]));
         for (int i = 0; i < 8000; i++) {
-            System.out.println(new User(i, "N" + i));
+           user = new User(i, "N" + i);
         }
-        Thread.sleep(600);
     }
 }
